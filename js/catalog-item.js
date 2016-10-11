647a37565f707af9c10b8307928c6ee5f4cb72fe
Artyom Resh <artyomresh@gmail.com>
2016-10-11 10:18:27 +0400

    Изменил название класса модального окна с размерами

--- js/catalog-item.js
+++ js/catalog-item.js
@@ -4,9 +4,9 @@ for (i = 0; i < cartBtn.length; i++) {
   cartBtn[i].addEventListener("click", function(event) {
     event.preventDefault();
     if (modalOverlay.classList.contains("modal-overlay--hidden")
-    && modalAddToCart.classList.contains("modal-add-to-cart--hidden")) {
+    && modalSizes.classList.contains("modal-sizes--hidden")) {
       modalOverlay.classList.remove("modal-overlay--hidden");
-      modalAddToCart.classList.remove("modal-add-to-cart--hidden");
+      modalSizes.classList.remove("modal-sizes--hidden");
     }
 });
 }
--- js/modal.js
+++ js/modal.js
@@ -1,30 +1,30 @@
 var modalOverlay = document.querySelector(".modal-overlay");
-var modalAddToCart = document.querySelector(".modal-add-to-cart");
-var modalForm = modalAddToCart.querySelector("form");
+var modalSizes = document.querySelector(".modal-sizes");
+var modalForm = modalSizes.querySelector("form");
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
     if (!modalOverlay.classList.contains("modal-overlay--hidden")
-    && !modalAddToCart.classList.contains("modal-add-to-cart--hidden")) {
+    && !modalSizes.classList.contains("modal-sizes--hidden")) {
       modalOverlay.classList.add("modal-overlay--hidden");
-      modalAddToCart.classList.add("modal-add-to-cart--hidden");
+      modalSizes.classList.add("modal-sizes--hidden");
     }
   }
 });
 
 modalOverlay.addEventListener("click", function(event) {
   if (!modalOverlay.classList.contains("modal-overlay--hidden")
-  && !modalAddToCart.classList.contains("modal-add-to-cart--hidden")) {
+  && !modalSizes.classList.contains("modal-sizes--hidden")) {
     modalOverlay.classList.add("modal-overlay--hidden");
-    modalAddToCart.classList.add("modal-add-to-cart--hidden");
+    modalSizes.classList.add("modal-sizes--hidden");
   }
 });
 
 modalForm.addEventListener("submit", function(event) {
   event.preventDefault();
   if (!modalOverlay.classList.contains("modal-overlay--hidden")
-  && !modalAddToCart.classList.contains("modal-add-to-cart--hidden")) {
+  && !modalSizes.classList.contains("modal-sizes--hidden")) {
     modalOverlay.classList.add("modal-overlay--hidden");
-    modalAddToCart.classList.add("modal-add-to-cart--hidden");
+    modalSizes.classList.add("modal-sizes--hidden");
   }
 });
